3cde7c56dce5ca49acc7653e599f693b7dcae650
MadiKABA <92320598+MadiKABA@users.noreply.github.com>
2022-09-18 13:14:27 +0000

    heroku configuration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,11 +10,13 @@ use Illuminate\Support\Facades\DB;
 class AuthController extends Controller
 {
     public function login(Request $request){
+
         $user= DB::table('utilisateurs')->where([
                 ['email', $request['email']],
                 ['password',$request['password']],
             ])->first();
-        if ($user){
+        //return response()->json($user);
+       if ($user){
             $userToken=Utilisateur::find($user->id);
             $token=$userToken->createToken('authToken')->accessToken;
            return [$token,$user];
